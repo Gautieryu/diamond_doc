029b9aed9a0e011c08a69f9deb52881170a43c9b
nzh <nzh20010713@163.com>
2022-05-22 18:16:08 +0800

    success vimword

--- src/views/VimWord.vue
+++ src/views/VimWord.vue
@@ -1,34 +1,22 @@
 <template>
-    <div>VimWord</div>
+    <div id="app">
+        <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+    </div>
 </template>
- 
+
 <script>
-    import CKEditor from '@ckeditor/ckeditor5-build-decoupled-document';
- 
+    import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
     export default {
-  data() {
-    return {
-      editor:null,//编辑器实例
+        name: 'app',
+        data() {
+            return {
+                editor: ClassicEditor,
+                editorData: '<p>Content of the editor.</p>',
+                editorConfig: {
+                    // The configuration of the editor.
+                }
+            };
+        }
     }
-  },
-  mounted() {
-     this.initCKEditor()
-  },
-   methods: {
-      initCKEditor() {
-        CKEditor.create(document.querySelector('#editor'), {
-          ckfinder: {
-            uploadUrl: '/admin/Upload/uploadUrl'
-            //后端处理上传逻辑返回json数据,包括uploaded(选项true/false)和url两个字段
-          }
-        }).then(editor => {
-          const toolbarContainer = document.querySelector('#toolbar-container');
-          toolbarContainer.appendChild(editor.ui.view.toolbar.element);
-          this.editor = editor //将编辑器保存起来，用来随时获取编辑器中的内容等，执行一些操作
-        }).catch(error => {
-          console.error(error);
-        });
-     }
-  }
-}
 </script>
