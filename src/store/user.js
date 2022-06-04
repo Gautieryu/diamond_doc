3970a779d55fe7b680e796d03a6140e675484b96
依夏 <1152084634@qq.com>
2022-06-04 13:18:34 +0800

    全局变量user(email)
    use this.$store.getters.getUser to get email
    use this.$store.dispatch(saveUserInfo/clearUserInfo,email) to store/clear

---
+++ src/store/user.js
@@ -0,0 +1,31 @@
+const key = 'user'
+const user = {
+    state() {
+        return {
+            user: ""
+        }
+    },
+    getters: {
+        getUser: state=>state.user
+    },
+    mutations: {
+        userStorage (state, value) {
+            state.user = value
+            localStorage.setItem(key, JSON.stringify(value))
+        },
+        removeStorage (state) {
+            state.user = null
+            localStorage.removeItem(key)
+        }
+    },
+    actions: {
+        saveUserInfo({ commit }, data) {
+            commit('userStorage', data)
+        },
+        clearUserInfo({ commit }) {
+            commit('removeStorage');
+        }
+    }
+};
+
+export default user
