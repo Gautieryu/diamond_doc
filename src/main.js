991d20b265cbc7c3d1ca1082af58a0c02a7eaa93
依夏 <1152084634@qq.com>
2022-06-04 13:12:10 +0800

    add axios by zhy

--- src/main.js
+++ src/main.js
@@ -3,16 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-
+import axios from 'axios';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import CKEditor from '@ckeditor/ckeditor5-vue2';
 
+Vue.prototype.$axios=axios;
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
-
 Vue.use( CKEditor );
+
+axios.defaults.baseURL = "http://101.43.141.16/";
 
 new Vue({
   router,
