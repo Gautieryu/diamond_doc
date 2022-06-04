500266a1c223d3f3e2abd9ec9b14fa95084573e8
依夏 <1152084634@qq.com>
2022-06-04 13:22:25 +0800

    Vuex模块化

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import user from './user'
+import group from './group'
+import text from './text'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+      user,
+      group,
+      text,
   }
 })
