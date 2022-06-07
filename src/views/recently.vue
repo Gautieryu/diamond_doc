ae79afcd61dc66f17e66650bfe76cd1fe889c30a
nzh <nzh20010713@163.com>
2022-06-07 22:47:25 +0800

    a

--- src/views/recently.vue
+++ src/views/recently.vue
@@ -90,6 +90,7 @@ export default {
         if(res.data.result==0)
         {
           this.$store.dispatch('text/saveText',res.data.personalFile);
+          this.$store.dispatch('file/saveFile',doc);
           window.open('#/VimWord', '_self');
         }
       }).catch(err=>{
@@ -104,6 +105,7 @@ export default {
         if(res.data.result==0)
         {
           this.$store.dispatch('text/saveText',res.data.groupFile);
+          this.$store.dispatch('file/saveFile',doc);
           window.open('#/VimWord', '_self');
         }
       }).catch(err=>{
