272cd4a9038d583efb7c8433cd1bb8fb6f3c75dc
依夏 <1152084634@qq.com>
2022-06-09 10:16:44 +0800

    Update AlterPosition.vue

--- src/components/gIchild/AlterPosition.vue
+++ src/components/gIchild/AlterPosition.vue
@@ -1,6 +1,6 @@
 <template>
-    <el-dialog width="400px" :visible="visible" :before-close="close">
-        <p>是否进行权限变更？</p>
+    <el-dialog title="确认修改" width="400px" :visible="visible" :before-close="close" custom-class="popup">
+        <span>确认进行权限变更？</span>
         <div slot="footer" class="dialog-footer">
             <el-button @click="close">取 消</el-button>
             <el-button type="warning" @click="realAlterP">确 定</el-button>
@@ -25,15 +25,31 @@ export default {
 }
 </script>
 
-<style scoped>
-    p
+<style>
+    .popup
     {
-        font-size: 30px;
+        border-radius: 6px;
+    }
+</style>
+<style scoped>
+    ::v-deep .el-dialog__header
+    {
+        font-size: 20px;
+        margin-left: 20px;
+        line-height: 20px;
+        font-weight: bold;
+    }
+    span
+    {
+        font-family: "PingFang SC","HarmonyOS_Regular";
+        font-size: 18px;
+        font-weight: normal;
         text-align: center;
         padding-top: 20px;
     }
     ::v-deep .el-dialog__body
     {
-        padding: 15px 20px;
+        padding: 15px 40px;
+        line-height: 40px;
     }
 </style>
